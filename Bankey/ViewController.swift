f24b0e75e08a9e08950911adbba21fefdec0814b
Anastasiya Omak <ios.developer.by@gmail.com>
2024-08-16 15:17:58 +0300

    Init commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Anastasiya Omak on 16/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
